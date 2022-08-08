fa633afa125ec5342135c04f705acfc2299ebfcb
sgteryn <sgteryn@gmail.com>
2022-08-08 12:18:22 -0400

    commit

--- functions.test.ts
+++ functions.test.ts
@@ -2,7 +2,7 @@ const {shuffleArray} = require('./utils')
 
 describe('shuffleArray should', () => {
     test("shuffleArray returns an array",() => {
-        expect(shuffleArray([1,2,3])).toBe([''])
+        expect(shuffleArray([1,2,3])).toBe(Array.isArray)
     }),
     test("shuffleArraycheck returns an array of the same length as the argument sent in",() => {
         expect(shuffleArray([0,4,3,4,5]).length).toBe(5)
